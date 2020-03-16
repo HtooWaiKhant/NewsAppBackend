82d685f06213295969c877d22482fcd1f6523ce0
htoowaikhant <htooowaikhant@gmail.com>
2020-03-16 09:44:22 +0630

    change fake image url

--- database/factories/Imagefactory.php
+++ database/factories/Imagefactory.php
@@ -8,7 +8,7 @@ use Faker\Generator as Faker;
 $factory->define(Image::class, function (Faker $faker) {
     return [
         'description' => $faker->sentence,
-        'url' => $faker->imageUrl(800, 600),
+        'url' => 'https://placeimg.com/400/300/any?' . rand(1, 100), //$faker->imageUrl(400, 400),
         'post_id' => $faker->numberBetween(1, 1500),
         'featured' => $faker->randomElement([true, false])
     ];
